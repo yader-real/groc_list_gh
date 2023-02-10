02df1f4167705782db966707107c941d44b1ac04
yader-real <yader.real@gmail.com>
2023-02-09 22:12:33 -0600

    GroceryList v1 with React

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = ({length}) => {
+    // const today = new Date();
+  return (
+    <footer>
+        <p>{length} List {length === 1 ? "item" :"items"}</p>
+    </footer>
+  )
+}
+
+export default Footer
